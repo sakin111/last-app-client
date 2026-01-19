11e9d6c87aaf559d5d4d5e44c4f0205b1011190a
sakin111 <maliksakin53@gmail.com>
2026-01-19 13:41:10 +0600

    feat: add travel plans page with dynamic rendering and subscription status check.

--- src/app/(commonLayout)/travel/page.tsx
+++ src/app/(commonLayout)/travel/page.tsx
@@ -1,17 +1,17 @@
 import TravelSection from "@/components/modules/Travels/TravelSection ";
 import { checkSubscription } from "@/services/subscribe/sub.service";
 
-// Mark this page as dynamic since it uses cookies
+
 export const dynamic = 'force-dynamic';
 
 const TravelPage = async () => {
 
     const checkSub = await checkSubscription();
     return (
-        <div className="bg-white min-h-screen">
-            <h1 className="text-center text-2xl font-semibold py-3 mt-5 dark:text-white">Explore Travel Plans</h1>
-            <div className="text-center w-36 h-px bg-blue-950 mx-auto"></div>
-            <TravelSection  checkSub={checkSub}/>
+        <div className="bg-background min-h-screen">
+            <h1 className="text-center text-2xl font-semibold py-3 mt-5 text-foreground">Explore Travel Plans</h1>
+            <div className="text-center w-36 h-px bg-primary mx-auto"></div>
+            <TravelSection checkSub={checkSub} />
         </div>
     );
 };
