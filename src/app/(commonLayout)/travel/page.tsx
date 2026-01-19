4f3685b55a0108c383f886dfb4c156022b855978
sakin111 <maliksakin53@gmail.com>
2026-01-19 13:21:44 +0600

    feat: add TravelPage component to display travel plans and check subscription status.

--- src/app/(commonLayout)/travel/page.tsx
+++ src/app/(commonLayout)/travel/page.tsx
@@ -9,7 +9,7 @@ const TravelPage = async () => {
     const checkSub = await checkSubscription();
     return (
         <div className="bg-white min-h-screen">
-            <h1 className="text-center text-2xl font-semibold py-3 mt-5 ">Explore Travel Plans</h1>
+            <h1 className="text-center text-2xl font-semibold py-3 mt-5 dark:text-white bg-black ">Explore Travel Plans</h1>
             <div className="text-center w-36 h-px bg-blue-950 mx-auto"></div>
             <TravelSection  checkSub={checkSub}/>
         </div>
